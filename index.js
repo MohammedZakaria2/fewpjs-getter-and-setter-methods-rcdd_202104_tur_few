6ad011b415291e8d0c8830da223848dadfba5aa3
Mohammed Zakaria Alalaya <mohammed.alalaya@gmail.com>
2021-06-23 02:04:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your Circle class here
+class Circle{
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get diameter() {
+    return this.radius * 2;
+  }
+  
+  get circumference() {
+    return 2 * this.radius * 3.14;
+  }
+  
+   get area() {
+    return this.radius * this.radius * 3.14;
+  }
+  
+    set phrase((radius) {
+    this.radius = radius
+  }
+}
+const circle = new Circle( 6 )
